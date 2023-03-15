6df85ddc753794953f45f335d1b05d97112fe3b3
megha gupta <megha.gupta@jungleegames.com>
2023-03-15 17:36:05 +0530

    hello

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"grpc_tut/chat"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+
+	lis, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := chat.Server{}
+	grpcServer := grpc.NewServer()
+
+	chat.RegisterChatServiceServer(grpcServer, &s)
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %s", err)
+	}
+}
